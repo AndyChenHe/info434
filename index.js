035205e0f45d263fb2404919b7f405b136ffda4e
He Chen <chenh57@uw.edu>
2019-10-25 22:32:09 -0700

    new version

--- index.js
+++ index.js
@@ -9,8 +9,8 @@
   window.onload = function() {
     svgContainer = d3.select('body')
       .append('svg')
-      .attr('width', 500)
-      .attr('height', 500);
+      .attr('width', 2000)
+      .attr('height', 2000);
     // d3.csv is basically fetch but it can be be passed a csv file as a parameter
     d3.csv("gapminder.csv")
       .then((data) => makeScatterPlot(data));
@@ -28,7 +28,7 @@
 
     // find data limits
     let axesLimits = findMinMax(fertility_rate_data, life_expectancy_data);
-
+    console.log(axesLimits);
     // draw axes and return scaling + mapping functions
     let mapFunctions = drawAxes(axesLimits, "fertility", "life_expectancy");
 
@@ -41,7 +41,6 @@
     var dropDown = d3.select("#filter").append("select")
       .attr("name", "country-list");
 
-      console.log(data[0].year);
 
     let allYear = []
     for (let i = 0; i < data.length; i ++){
@@ -80,44 +79,47 @@
       svgContainer.selectAll("circle")
           .filter(function(d) {return selected == d.year;})
           .attr("display", display);
-
       });
   }
 
   // make title and axes labels
   function makeLabels() {
     svgContainer.append('text')
-      .attr('x', 100)
-      .attr('y', 40)
-      .style('font-size', '14pt')
+      .attr('x', 300)
+      .attr('y', 50)
+      .style('font-size', '20pt')
       .text("Countries by Life Expectancy and Fertility Rate");
 
     svgContainer.append('text')
-      .attr('x', 130)
-      .attr('y', 490)
-      .style('font-size', '10pt')
+      .attr('x', 400)
+      .attr('y', 600)
+      .style('font-size', '15pt')
       .text('Fertility Rates (Avg Children per Woman)');
 
     svgContainer.append('text')
-      .attr('transform', 'translate(15, 300)rotate(-90)')
-      .style('font-size', '10pt')
+      .attr('transform', 'translate(30, 400)rotate(-90)')
+      .style('font-size', '15pt')
       .text('Life Expectancy (years)');
   }
 
   // plot all the data points on the SVG
   // and add tooltip functionality
   function plotData(map) {
+    
     // get population data as array
     let pop_data = data.map((row) => +row["population"]);
     let pop_limits = d3.extent(pop_data);
+    console.log(pop_limits);
     // make size scaling function for population
     let pop_map_func = d3.scaleLinear()
       .domain([pop_limits[0], pop_limits[1]])
-      .range([3, 20]);
+      .range([3, 80]);
 
     // mapping functions
     let xMap = map.x;
     let yMap = map.y;
+    console.log('hi')
+    console.log(xMap)
 
     // make tooltip
     let div = d3.select("body").append("div")
@@ -134,13 +136,19 @@
         .attr('cx', xMap)
         .attr('cy', yMap)
         .attr('r', (d) => pop_map_func(d["population"]))
-        .attr('fill', "#4286f4")
+        .style('fill', 'none')
+        .style('stroke', 'rgb(93,120,164)')
+        .style("stroke-width", 3) 
         // add tooltip functionality to points
         .on("mouseover", (d) => {
           div.transition()
-            .duration(200)
+            .duration(10)
             .style("opacity", .9);
-          div.html(d.country + "<br/>" + numberWithCommas(d['population']))
+          div.html(d.country + "<br/>" + 'year: ' +numberWithCommas(d['year']) 
+                    + "<br/>" + 'Life expectancy: ' +numberWithCommas(d['life_expectancy']) 
+                    + "<br/>" + 'Fertility: ' +numberWithCommas(d['fertility']) 
+                    + "<br/>" + 'Population: ' +numberWithCommas(d['population']) 
+                    )
             .style("left", (d3.event.pageX) + "px")
             .style("top", (d3.event.pageY - 28) + "px");
         })
@@ -159,7 +167,7 @@
     // function to scale x value
     let xScale = d3.scaleLinear()
       .domain([limits.xMin - 0.5, limits.xMax + 0.5]) // give domain buffer room
-      .range([50, 450]);
+      .range([100, 1150]);
 
     // xMap returns a scaled x value from a row of data
     let xMap = function(d) { return xScale(xValue(d)); };
@@ -167,7 +175,7 @@
     // plot x-axis at bottom of SVG
     let xAxis = d3.axisBottom().scale(xScale);
     svgContainer.append("g")
-      .attr('transform', 'translate(0, 450)')
+      .attr('transform', 'translate(0, 550)')
       .call(xAxis);
 
     // return y value from a row of data
@@ -176,7 +184,7 @@
     // function to scale y
     let yScale = d3.scaleLinear()
       .domain([limits.yMax + 5, limits.yMin - 5]) // give domain buffer
-      .range([50, 450]);
+      .range([50, 550]);
 
     // yMap returns a scaled y value from a row of data
     let yMap = function (d) { return yScale(yValue(d)); };
@@ -184,7 +192,7 @@
     // plot y-axis at the left of SVG
     let yAxis = d3.axisLeft().scale(yScale);
     svgContainer.append('g')
-      .attr('transform', 'translate(50, 0)')
+      .attr('transform', 'translate(100, 0)')
       .call(yAxis);
 
     // return mapping and scaling functions
